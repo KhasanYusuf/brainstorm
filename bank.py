508d7abaf22fd0518a23877b182f2b0d9bc46e47
KhasanYusuf <113286168+KhasanYusuf@users.noreply.github.com>
2022-09-17 19:24:32 +0700

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,8 +1,7 @@
-greet=input(' ').lower().replace(" ","")
-x=greet[0:5]
+greet=input('').lower().replace(" ","")
 if(greet=='hello'):
     money=0
-elif(x=='hello'):
+elif(greet[0:5]=='hello'):
     money=0
 elif(greet[0]=='h'):
     money=20
@@ -10,3 +9,4 @@ else :
     money=100
 
 print ('$',money)
+
